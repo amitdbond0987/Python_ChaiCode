645eee81ad01c408bd44a8504bc74b291b17cae9
AMIT KUSHWAHA <76873372+amitdbond0987@users.noreply.github.com>
2024-08-26 12:30:11 +0000

    solution2

--- solution2.py
+++ solution2.py
@@ -1,3 +1,18 @@
-a = 5
-b=6
-print(a+b)
+age=int(input("Enter your age : "))
+day=input("Enter day : ")
+low=day.lower()
+
+price = 12 if age>18 else 8
+
+if low == "wednesday":
+    if age > 18:
+        price = price-2
+    else:
+        price = price-2
+else:
+    if age>18:
+        price = 12
+    else:
+        price = 8
+
+print(price)
