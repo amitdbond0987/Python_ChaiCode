48a8ec3e5a2d59a67f80920499c2f27be2fa6e24
AMIT KUSHWAHA <76873372+amitdbond0987@users.noreply.github.com>
2024-08-26 11:52:51 +0000

    solution is now correcrt

--- solution1.py
+++ solution1.py
@@ -1,3 +1,12 @@
-age= input("Enter your age : ")
-age_int = int(age)
+age= int(input("Enter your age : "))
+
+if age < 13:
+    print("child")
+elif age < 20:
+    print ("Teen")
+elif age < 60:
+    print("Adult")
+else:
+    print("Senior")
+
 
